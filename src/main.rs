6158147eed3ae81bf99d4fe88b0697b52d4a711d
kartik kanugo <kartik.kanugo@vectoflow.de>
2023-12-29 23:10:13 +0530

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,15 +9,22 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_success_connect(_stream);
+                println!("successful connection");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_success_connect(mut _stream: TcpStream) {
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n");
+    _stream.write_all(response.as_bytes()).unwrap();
 }
