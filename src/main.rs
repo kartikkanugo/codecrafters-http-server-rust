a92276ddbe8a18ae5bcf829d5a9568c4dc6d6ff7
kartik kanugo <kartik.kanugo@vectoflow.de>
2024-01-06 22:22:53 +0530

    working threads

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,102 @@
 // Uncomment this block to pass the first stage
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
+const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-    // Uncomment this block to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Request {
+    method: String,
+    path: String,
+    http_version: String,
+    host: String,
+    agent: String,
+}
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-                handle_success_connect(_stream);
-                println!("successful connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+fn parse_requests(stream: &mut TcpStream) -> Request {
+    let buf = &mut vec![0; 1024];
+    stream.read(buf).unwrap(); // reads the data to the buffer
+    let req_str = String::from_utf8_lossy(buf);
+    println!("printing cow string\n {} ", req_str); // println takes no ownership and uses references
+    let req: Vec<Vec<&str>> = req_str
+        .split("\r\n")
+        .map(|line| line.split(" ").collect())
+        .collect();
+    let rest: Vec<&str> = req[1..req.len() - 2]
+        .iter()
+        .map(|x| match x.get(1) {
+            Some(s) => s.to_owned(),
+            None => "",
+        })
+        .collect();
+
+    let data: Vec<&str> = match req.get(0) {
+        Some(s) => s.to_owned(),
+        None => Vec::new(),
+    }; // only first values are different
+
+    println!("{:?} {:?}", data, rest);
+
+    let host = match rest.get(3) {
+        Some(s) => s,
+        None => "",
+    };
+
+    let agent = match rest.get(0) {
+        Some(s) => s,
+        None => "",
+    };
+
+    // return a request
+    Request {
+        method: data.get(0).unwrap().to_string(),
+        path: data.get(1).unwrap().to_string(),
+        http_version: data.get(2).unwrap().to_string(),
+        host: host.to_string(),
+        agent: agent.to_string(),
     }
 }
 
-fn handle_success_connect(mut _stream: TcpStream) {
-    // read the data here important to preallocate else it reads till infinity
-    let mut request_data_vec: Vec<u8> = vec![0; 1000];
-    match _stream.read(request_data_vec.as_mut_slice()) {
-        Ok(_) => {
-            // check the request data vec
-            println!(
-                "buffer: {}",
-                String::from_utf8_lossy(request_data_vec.as_slice())
-            );
+fn make_response(content: &str) -> String {
+    let con = format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        content.len(),
+        content
+    );
+    con
+}
 
-            // Now lets accept the string if it only contains / marker after the local host
-            let string_data = String::from_utf8_lossy(request_data_vec.as_slice());
-            // now get lines
-            let line_data: Vec<&str> = string_data.split("\r\n").collect();
-            // now in the first line there is get method or some method with some html link
-            let start_line = line_data[0];
-            let start_parts: Vec<&str> = start_line.split(" ").collect();
-            let path = start_parts[1];
-            if path == "/" {
-                let response = format!("HTTP/1.1 200 OK\r\n\r\n");
-                _stream.write_all(response.as_bytes()).unwrap();
-            } else if path.starts_with("/echo/") {
-                let paths = path.strip_prefix("/echo/").unwrap();
-                let response = format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    paths.len().to_string(),
-                    paths
-                );
-                _stream.write_all(response.as_bytes()).unwrap();
-            } else {
-                let response = format!("HTTP/1.1 404 Not Found\r\n\r\n");
-                _stream.write_all(response.as_bytes()).unwrap();
+fn main() {
+    println!("logs appear here");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    for stream in listener.incoming() {
+        thread::spawn(move || match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                let req = parse_requests(&mut stream);
+                println!("{:?}", req);
+                // now all are parsed in the struct according to requirements
+                let path_split: Vec<&str> = req.path.as_str().split("/").collect();
+                let resource = path_split.get(1).unwrap();
+                let mut path = String::from("/");
+                path.push_str(&resource);
+                match path.as_str() {
+                    "/" => stream.write_all(OK_RESPONSE.as_bytes()).unwrap(),
+                    "/echo" => stream
+                        .write_all(make_response(path_split[2..].join("/").as_str()).as_bytes())
+                        .unwrap(),
+                    "/user-agent" => stream
+                        .write_all(make_response(&req.agent).as_bytes())
+                        .unwrap(),
+                    _ => stream.write_all(NOT_FOUND_RESPONSE.as_bytes()).unwrap(),
+                }
             }
-        }
-        Err(_err) => {
-            // Nothing to do here
-        }
-    };
-
-    // send the response here
+            Err(e) => {
+                println!("error something wrong {}", e);
+            }
+        });
+    }
 }
